5c549aaaf7ee2c2355905e0ceba491a3fa524e42
Ian Denev <contact@iandenev.com>
2021-10-19 21:03:28 +0100

    Test added: argument must be a Number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const normalToRoman = number => {
     if (number == undefined) throw new Error("an argument is required");
+    else if (number.constructor != "Number") throw new Error("argument must be a Number");
 }
 
 const romanToNormal = number => {
--- index.test.js
+++ index.test.js
@@ -4,4 +4,7 @@ describe("normalToRoman testing suite", () => {
     test("an argument is required", () => {
         expect(() => normalToRoman()).toThrow("an argument is required");
     })
+    test("argument is a Number", () => {
+        expect(() => normalToRoman("str")).toThrow("argument must be a Number");
+    })
 })
